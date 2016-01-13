df1d05d2ebbbb4f857d1e53f68256a18274ff9c2
George Lamprianidis <giorgos.lamprianidis@gmail.com>
2016-01-13 18:08:12 -0500

    Change db

--- app/dbclient.rb
+++ app/dbclient.rb
@@ -6,19 +6,20 @@ class DbClient
   def connect(opts)
     opts.reject! { |k, v| v.to_s.empty? }
     host = (opts.values_at(:mongo_host, "mongo_host") + ["127.0.0.1:27017"]).compact.first
-    @db  = (opts.values_at(:mongo_db, "mongo_db") + ["tmp"]).compact.first
+    db   = (opts.values_at(:mongo_db, "mongo_db") + ["tmp"]).compact.first
     user = (opts.values_at(:mongo_user, "mongo_user")).compact.first
     pass = (opts.values_at(:mongo_pass, "mongo_pass")).compact.first
-    connection_options = {database: @db}
+    connection_options = {database: db}
     if !user.nil? && !pass.nil?
       connection_options[:user] = user
       connection_options[:password] = pass
       connection_options[:auth_mech] = :plain
     end
-    if @client.nil? || server_address != host
+    if @client.nil? || server_address != host || db != @db
       @client.try(:close)
       @client = Mongo::Client.new([host], connection_options)
     end
+    @db = db
   end
 
   def close

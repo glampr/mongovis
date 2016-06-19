9fe88c73c6f966baef62566e70ab79125fbeb99f
George Lamprianidis <giorgos.lamprianidis@gmail.com>
2016-06-19 19:28:41 -0400

    Update bulk writer

--- app/bulk_writer.rb
+++ app/bulk_writer.rb
@@ -35,3 +35,19 @@ class BulkWriter
   end
 
 end
+
+class Hash
+
+  def deep_symbolize_keys
+    deep_transform_keys{ |key| key.to_sym rescue key }
+  end
+
+  def deep_transform_keys(&block)
+    result = {}
+    each do |key, value|
+      result[yield(key)] = value.is_a?(Hash) ? value.deep_transform_keys(&block) : value
+    end
+    result
+  end
+
+end
